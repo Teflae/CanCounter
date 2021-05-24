22a3018700d2059f6c38a6c8227bb6ffb0637fe9
alphax10 <pauljis@outlook.com>
2021-05-24 20:51:09 +0930

    Standard
    Created a new git ignore and then refined the structure of CC.ino

--- CC/CC.ino
+++ CC/CC.ino
@@ -1,36 +1,83 @@
 /*
-  AnalogReadSerial
-
-  Reads an analog input on pin 0, prints the result to the Serial Monitor.
-  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogReadSerial
+* Rules:
+Don't use underscores plz, there reserved for blanks.
+Names should be formatted vague-to-specific, eg my addres is EarthAusSA5092Leda3
+Capital case for global variables, and lower case for local variables
+Comment everywhare for future people
 */
-#define Dampen 10
-#define Avg 500
+// Settings. Keep in A-Z order or in files that are relavent, prefixed with 'const' with CamelCase.
+const double Dampen = 20.0;
+const double DampenLong = 2000.0;
+const double DebugTimeut = 10000;
+const String DebugMessage = "> CanCounter.ino | V0.1 | Type 'help' for help";
+const unsigned long SerialBaudRate = 57600;
+const uint8_t PinLightSensor = A0;
+
+// Global Variables
 double avg = 1;
 double damp = 1;
-// the setup routine runs once when you press reset:
-void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(56000);
+bool Debugging = false;
+unsigned long DebugEnd = 0;
+
+void setup()
+{
+	Serial.begin(SerialBaudRate); // Init serial @ 56kHz
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A1);
-  damp = (damp * (Dampen - 1) + sensorValue)/Dampen;
-  avg = (avg * (Avg - 1) + sensorValue)/Avg;
-  // print out the value you read:
-  Serial.print(sensorValue);
-  Serial.print('|');
-  Serial.print(damp);
-  Serial.print('|');
-  Serial.print(avg);
-  Serial.println();
-  delay(10);        // delay in between reads for stability
+void loop()
+{
+	if (Debugging)
+	{ // Debug Mode
+		if (Serial.available() > 0) // Recived command
+		{
+			// Get message
+			String message;
+			while (Serial.available() > 0)
+			{
+				char in = Serial.read();
+				if (in == '\n') break; // '\n' is message seperator
+				message += in; // Add to buffer
+				delay(10); // Wait for more chars
+			}
+
+			// Seperate words
+			int i = 0;
+			int n = 0;
+      //Serial.println(message.indexOf(' ')); //TEST
+			while (i != -1)
+			{
+				i = message.indexOf(' ', i + 1);  
+				n++;
+			}
+      Serial.println(n); //TEST
+
+			
+			DebugEnd = millis() + DebugTimeut; // Reset timeout
+		}
+		if (millis() > DebugEnd)
+		{
+			Debugging = false;
+		}		
+		delay(50);
+	}
+	else
+	{ // Normal Mode
+		// read the input on analog pin 0:
+		int sensorValue = analogRead(PinLightSensor);
+		damp = (damp * (Dampen - 1) + sensorValue)/Dampen;
+		avg = (avg * (DampenLong - 1) + sensorValue)/DampenLong;
+		// print out the value you read:
+		Serial.print(sensorValue);
+		Serial.print('|');
+		Serial.print(damp);
+		Serial.print('|');
+		Serial.print(avg);
+		Serial.println();
+		if (Serial.available() > 0) {
+			Debugging = true;
+			DebugEnd = millis() + DebugTimeut;
+			Serial.println(DebugMessage);
+		}		
+		delay(100);
+	}
 }
